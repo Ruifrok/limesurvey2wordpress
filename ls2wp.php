34292502838a12d5f28ff3a2680f6236bc00f0b1
Rob Ruifrok <robruifrok@multiweb.nl>
2025-01-08 16:55:15 +0100

    version change

--- ls2wp.php
+++ ls2wp.php
@@ -10,7 +10,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:		  ls2wp
  * Domain Path:		  /languages
- * Version:			  0.5				
+ * Version:			  1.0			
  * Requires Plugins:  
  */
  
